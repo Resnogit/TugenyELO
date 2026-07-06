5bd0d53c4baafa466e9e225d338ef01776e5489f
Resnogit <marius.ritter@posteo.de>
2026-07-06 13:54:57 +0200

    rebuilt json export functions into single functions

--- build_dict.py
+++ build_dict.py
@@ -6,33 +6,25 @@ path_teams_json = "json/2025_teams.json"
 path_matches_json = "json/2025_matches.json"
 
 
-def teams_json_to_dict(path):
+def build_teams_db(path):
+    team_class_list = []  # New list container for resulting list.
     with open(path) as data:
         data_str = data.read()  # Reads path into a string
     dict_from_json = json.loads(data_str)  # creates dict object from json text
-    return dict_from_json["teams"]
-
-
-def teams_dict_to_class(dict):
-    team_class_list = []  # New list container
-    for team_dict in dict:
-        new_team = team(
-            team_dict["id"], team_dict["name"]
-        )  # Iterates over Teams.json list g
+    for team_dict in dict_from_json[
+        "teams"
+    ]:  # iterates over the new dict, only moving selected items to resulting teams list
+        new_team = team(team_dict["id"], team_dict["name"])
         team_class_list.append(new_team)
     return team_class_list
 
 
-def match_json_to_dict(path):
+def build_match_db(path):
+    match_class_list = []  # Empty list for result
     with open(path) as data:
         data_str = data.read()  # Reads path into a string
     dict_from_json = json.loads(data_str)  # creates dict object from json text
-    return dict_from_json
-
-
-def match_dict_to_class(dict):
-    match_class_list = []
-    for match_dict in dict:
+    for match_dict in dict_from_json:
         new_match = match(
             match_dict["first_team"],
             match_dict["first_team_id"],
@@ -45,5 +37,5 @@ def match_dict_to_class(dict):
     return match_class_list
 
 
-# list_of_teams = teams_dict_to_class(teams_json_to_dict((path_teams_json)))
-# list_of_matches = match_dict_to_class(match_json_to_dict(path_matches_json))
+# list_of_teams = build_teams_db(path_teams_json)
+list_of_matches = build_match_db(path_matches_json)
