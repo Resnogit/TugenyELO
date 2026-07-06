d94f2e5ce1604ddf0571293d69e9733fcd0c4c71
Resnogit <marius.ritter@posteo.de>
2026-07-06 13:38:33 +0200

    updated match class

--- classes.py
+++ classes.py
@@ -1,11 +1,22 @@
 class match:
-    def __init__(self, first_team, second_team, victorious_team) -> None:
-        self.__first_team = first_team
-        self.__second_team = second_team
-        self.__victorious_team = victorious_team
+    def __init__(
+        self,
+        first_team,
+        first_team_id,
+        second_team,
+        second_team_id,
+        victorious_team,
+        victorious_team_id,
+    ) -> None:
+        self.first_team = first_team
+        self.__first_team_id = first_team_id
+        self.second_team = second_team
+        self.__second_team_id = second_team_id
+        self.victorious_team = victorious_team
+        self.__victorious_team_id = victorious_team_id
 
     def get_winner(self):
-        return self.__victorious_team
+        return self.victorious_team
 
 
 class team:
