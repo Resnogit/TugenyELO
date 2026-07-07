ffd9139d58dc74866fae3cbed8017a9538d08af6
Resnogit <marius.ritter@posteo.de>
2026-07-07 15:23:47 +0200

    basic match/win/winrate calc added

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-import json
+from build_dict import build_match_db, build_teams_db
+from classes import match, team
 
-import calls
-from classes import match
+path_teams_json = "json/2025_teams.json"
+path_matches_json = "json/2025_matches.json"
+
+
+def calculate_elo(match_db, team_db):
+    for match in match_db:
+        team_1 = match.first_team
+        team_2 = match.second_team
+        winner = match.get_winner()
+        for team in team_db:
+            if team.name == team_1 or team.name == team_2:
+                team.matches += 1
+            if team.name == winner:
+                team.wins += 1
+    for team in team_db:
+        winrate = team.get_winrate()
+        print(
+            f"{team.name}:{team.matches} matches; {team.wins} won. Winrate: {winrate}"
+        )
 
 
 def main():
-    # teams_json = calls.get_teams()
-    # all_teams = json.loads(teams_json)
-    # print(all_teams)
-    # match_1 = match("Team1", "Team2", "Team1")
-    # print(match_1.get_winner())
+    team_db = build_teams_db(path_teams_json)
+    match_db = build_match_db(path_matches_json)
+    calculate_elo(match_db, team_db)
 
 
 main()
