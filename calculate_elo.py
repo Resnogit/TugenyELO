5a5270b8e4f36dd9243edaa1d0982b69dc3a0555
Resnogit <marius.ritter@posteo.de>
2026-07-21 06:00:52 +0200

    work on calculate_elo

--- calculate_elo.py
+++ calculate_elo.py
@@ -6,11 +6,21 @@ def calculate_elo(match_db):
         match_id = game.get_id()
         winner = game.get_winner()
         looser = game.get_looser()
-        counter = 1
+        match_counter = 1
+        winner_wins = 1
+        looser_wins = 0
+        ## CHECK previous games
         for each in counted_matches:
             teams = (each.first_team, each.second_team)
             if winner in teams and looser in teams:
-                counter+=1
+                match_counter += 1
+                if winner == each.victorious_team:
+                    winner_wins += 1
+                else:
+                    looser_wins += 1
+        ##
+
+
         counted_matches.add(game)
         print(f"Parsing Match #{match_id}: Won: {winner} Lost: {looser} ({counter} matches between teams)")
 
